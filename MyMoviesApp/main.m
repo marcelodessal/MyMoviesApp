63ed09532a3ae66226e61e81615335990ef9ec9e
Marcelo Dessal <marcelodessal@Marcelos-MacBook-Pro.local>
2015-03-09 19:49:09 -0300

    Initial Commit

---
+++ MyMoviesApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMoviesApp
+//
+//  Created by Marcelo Dessal on 3/9/15.
+//  Copyright (c) 2015 Marcelo Dessal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
